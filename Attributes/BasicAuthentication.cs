a3f7e4e1050b8619fd89cdb0961129eed63c6251
Fatih HAS <fatihhas2609@gmail.com>
2023-04-11 16:56:06 +0300

    First Commit
    WebAPI BasicAuth

---
+++ Attributes/BasicAuthentication.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebAPI.Attributes
+{
+    public class BasicAuthentication : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.Headers.Authorization != null)
+            {
+                var authToken = actionContext.Request.Headers.Authorization.Parameter;
+                var decodeauthToken = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(authToken));
+                var arrUserNameandPassword = decodeauthToken.Split(':');
+
+                if (IsAuthorizedUser(arrUserNameandPassword[0], arrUserNameandPassword[1]))  
+                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(arrUserNameandPassword[0]), null);
+                else
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+            }
+            else
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+        }
+        public static bool IsAuthorizedUser(string Username, string Password)
+        {
+            // In this method we can handle our database logic here...  
+            return Username == "fatihh" && Password == "test";
+        }
+    }
+}
---
+++ Controllers/EmptyController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebAPI.Attributes;
+
+namespace WebAPI.Controllers
+{
+    public class EmptyController : ApiController
+    {
+        // GET: Empty
+        [BasicAuthentication]
+        public string Get()
+        {
+            return "FatihHAS";
+        }
+
+        [BasicAuthentication]
+        public List<string> Get(int id)
+        {
+            return new List<string>()
+            {
+                "RestfulAPI","HelloWorld"
+            };
+        }
+    }
+}
